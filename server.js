a02f38dd2220655b96a88fb54f9c592fe280219c
Andrew Njoo <adnjoo@gmail.com>
2021-07-20 10:28:16 -0700

    edit

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-//import express for server endpoint
-const express = require('express')
-
+//declare variables / import dependencies
+const express = require("express");
+const path = require("path");
 //make class Rollbar from rollbar
-const Rollbar = require('rollbar')
+const Rollbar = require("rollbar");
+const port = process.env.port || 4000;
 
-const port = process.env.port || 4000
-
+//instantiate an instance of Rollbar class below
 const rollbar = new Rollbar({
-  accessToken: 'be02e119ecd545858db7b5ccd725fc39',
+  accessToken: "be02e119ecd545858db7b5ccd725fc39",
   captureUncaught: true,
-  captureUnhandledRejections: true
+  captureUnhandledRejections: true,
 });
+
+const app = express();
+app.use(express.json());
+app.use(rollbar.errorHandler());
+
+let heroesList = [];
 
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
-const app = express();
-app.use(express.json())
-app.use(rollbar.errorHandler())
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'))
+  // functiondoesntexist()
+  rollbar.info("test worked");
+});
 
-app.get('/',(req,res)=>{
-  res.status(200).send('good job')
-  rollbar.info('test worked')
-})
+app.post("/api/hero", (req, res) => {
+  let { hero } = req.body;
+  hero = hero.trim();
 
-app.listen(port, ()=>{
-  console.log(`server listening on port ${port}`)
-})
+  const index = heroesList.findIndex((name) => {
+    return name === hero;
+  });
+
+  if (index === -1 && hero !== "") {
+    heroesList.push(hero);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
