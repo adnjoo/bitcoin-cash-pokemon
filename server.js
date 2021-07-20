f296d21b0dd3d9996b15874c77461ae930b62db0
Andrew Njoo <adnjoo@gmail.com>
2021-07-20 10:12:21 -0700

    edit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express')
 //make class Rollbar from rollbar
 const Rollbar = require('rollbar')
 
+const port = process.env.port || 4000
+
 const rollbar = new Rollbar({
   accessToken: 'be02e119ecd545858db7b5ccd725fc39',
   captureUncaught: true,
@@ -12,3 +14,16 @@ const rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
+
+const app = express();
+app.use(express.json())
+app.use(rollbar.errorHandler())
+
+app.get('/',(req,res)=>{
+  res.status(200).send('good job')
+  rollbar.info('test worked')
+})
+
+app.listen(port, ()=>{
+  console.log(`server listening on port ${port}`)
+})
