e0a84596dba23ecc9281648b28ad92322132e0ff
Andrew Njoo <adnjoo@gmail.com>
2021-07-20 12:31:43 -0700

    edit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 //make class Rollbar from rollbar
 const Rollbar = require("rollbar");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5500;
 
 //instantiate an instance of Rollbar class below
 const rollbar = new Rollbar({
